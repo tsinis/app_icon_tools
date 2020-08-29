a287a51078e2ffb91a757506e63e3691f714b3ce
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2020-08-29 20:17:14 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/is/tsin/launcher_icons_gui/launcher_icons_gui/MainActivity.kt
@@ -0,0 +1,6 @@
+package is.tsin.launcher_icons_gui.launcher_icons_gui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) => MaterialApp(home: Container());
+}
