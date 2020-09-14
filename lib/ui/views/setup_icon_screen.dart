5f1966d66435229a838b333b0008762b1d723215
Roman Cinis <52065414+tsinis@users.noreply.github.com>
2020-09-15 00:12:55 +0200

    Changed Layout of Icon Preview

--- lib/ui/views/setup_icon_screen.dart
+++ lib/ui/views/setup_icon_screen.dart
@@ -1,63 +1,90 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:launcher_icons_gui/ui/platform_icons/icon.dart';
 import 'package:websafe_svg/websafe_svg.dart';
 
-import '../custom_pageviews/background_view.dart';
-import '../custom_pageviews/icon_view.dart';
+import '../platform_icons/icon.dart';
 import '../platform_icons/platforms_list.dart';
 import '../widgets/adaptive/scaffold_app_bar.dart';
+import '../widgets/layout.dart';
 
 class SetupScreen extends StatelessWidget {
   const SetupScreen({Key key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final bool _portrait = MediaQuery.of(context).size.height > MediaQuery.of(context).size.width;
     // print('Second Screen build');
     return AdaptiveScaffold(
-      child: SnapClipPageView(
-        backgroundBuilder: _buildBackground,
-        itemBuilder: _buildChild,
-        length: platformList.length,
-        backgroundDecoration: BoxDecoration(
-          gradient: LinearGradient(
-              colors: [Colors.grey[600], Colors.transparent],
-              begin: Alignment.bottomCenter,
-              end: Alignment.topCenter,
-              stops: const [.1, .5]),
-        ),
-      ),
-    );
-  }
-
-  BackgroundWidget _buildBackground(BuildContext context, int index) => BackgroundWidget(
-        index: index,
-        child: Transform.scale(
-          alignment: const Alignment(0, -0.7),
-          scale: 0.7,
-          child: FittedBox(
+        child: Center(
+      child: PreviewLayout(
+        portraitOrientation: _portrait,
+        children: [
+          platformList[1],
+          FittedBox(
             fit: BoxFit.contain,
-            child: Stack(
-              alignment: Alignment.center,
+            child: Row(
               children: [
-                WebsafeSvg.asset(platformList[index].devicePicture, fit: BoxFit.contain, height: 640),
-                SizedBox(
-                    height: 40,
-                    width: 40,
-                    child: IconWithShape(onDevice: true, supportTransparency: platformList[index].platformID != 2)),
+                // const SizedBox(width: 60),
+                Stack(
+                  alignment: Alignment.center,
+                  children: [
+                    WebsafeSvg.asset(platformList[0].devicePicture, fit: BoxFit.contain, height: 640),
+                    SizedBox(
+                        height: 40,
+                        width: 40,
+                        child: IconWithShape(onDevice: true, supportTransparency: platformList[1].platformID != 2)),
+                  ],
+                ),
               ],
             ),
-          ),
-        ),
-      );
+          )
+        ],
+      ),
+    )
 
-  PageViewItem _buildChild(BuildContext context, int index) => PageViewItem(
-      buildDecoration: _decoration,
-      padding: const EdgeInsets.all(0),
-      margin: const EdgeInsets.all(40),
-      height: 340,
-      index: index,
-      child: platformList[index]);
+        // SnapClipPageView(
+        //   backgroundBuilder: _buildBackground,
+        //   itemBuilder: _buildChild,
+        //   length: platformList.length,
+        //   backgroundDecoration: BoxDecoration(
+        //     gradient: LinearGradient(
+        //         colors: [Colors.grey[600], Colors.transparent],
+        //         begin: Alignment.bottomCenter,
+        //         end: Alignment.topCenter,
+        //         stops: const [.1, .5]),
+        //   ),
+        // ),
+        );
+  }
 
-  Decoration _decoration(double _) => const BoxDecoration(color: Colors.transparent);
+  // BackgroundWidget _buildBackground(BuildContext context, int index) => BackgroundWidget(
+  //       index: index,
+  //       child: Transform.scale(
+  //         alignment: const Alignment(0, -0.7),
+  //         scale: 0.7,
+  //         child: FittedBox(
+  //           fit: BoxFit.contain,
+  //           child: Stack(
+  //             alignment: Alignment.center,
+  //             children: [
+  //               WebsafeSvg.asset(platformList[index].devicePicture, fit: BoxFit.contain, height: 640),
+  //               SizedBox(
+  //                   height: 40,
+  //                   width: 40,
+  //                   child: IconWithShape(onDevice: true, supportTransparency: platformList[index].platformID != 2)),
+  //             ],
+  //           ),
+  //         ),
+  //       ),
+  //     );
+
+  // PageViewItem _buildChild(BuildContext context, int index) => PageViewItem(
+  //     buildDecoration: _decoration,
+  //     padding: const EdgeInsets.all(0),
+  //     margin: const EdgeInsets.all(40),
+  //     height: 340,
+  //     index: index,
+  //     child: platformList[index]);
+
+  // Decoration _decoration(double _) => const BoxDecoration(color: Colors.transparent);
 }
---
+++ lib/ui/widgets/layout.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/widgets.dart';
+
+class PreviewLayout extends StatelessWidget {
+  const PreviewLayout({Key key, this.children, this.portraitOrientation}) : super(key: key);
+
+  final List<Widget> children;
+  final bool portraitOrientation;
+
+  @override
+  Widget build(BuildContext context) => portraitOrientation
+      ? FractionallySizedBox(
+          widthFactor: 0.9,
+          child: SingleChildScrollView(
+            child: Column(
+                mainAxisSize: MainAxisSize.max, mainAxisAlignment: MainAxisAlignment.spaceAround, children: children),
+          ),
+        )
+      : FractionallySizedBox(
+          widthFactor: 0.9,
+          heightFactor: 0.9,
+          child: Row(
+              mainAxisSize: MainAxisSize.max,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: children),
+        );
+}
