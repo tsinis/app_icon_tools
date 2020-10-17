13849206fb5e51c350d078ed35c178e0ebb5f239
tsinis <52065414+tsinis@users.noreply.github.com>
2020-10-17 20:47:48 +0200

    Added ZIP archive (with icons) saving to Downloads folder on macOS

--- lib/models/setup_icon.dart
+++ lib/models/setup_icon.dart
@@ -1,12 +1,11 @@
 import 'dart:io' as io;
-// import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/widgets.dart';
 import 'package:image_resizer/image_resizer.dart';
 import 'package:image/image.dart' as img;
-// import 'package:path_provider/path_provider.dart';
+import 'package:path_provider/path_provider.dart';
 import 'package:universal_html/html.dart' as html;
 
 import '../locator.dart';
@@ -43,6 +42,7 @@ class SetupIcon extends ChangeNotifier {
   set icon(Uint8List _uploadedImage) {
     if (_uploadedImage != _icon) {
       _icon = _uploadedImage;
+      _iconFiles = {};
       notifyListeners();
     }
   }
@@ -101,7 +101,7 @@ class SetupIcon extends ChangeNotifier {
     }
   }
 
-  Map<String, List<FileData>> _files;
+  Map<String, List<FileData>> _iconFiles;
 
   // final bool _exportIos = true, _exportWeb = true, _exportMacos = true, _exportAndroid = true;
 
@@ -110,8 +110,8 @@ class SetupIcon extends ChangeNotifier {
     final IconGenerator _gen = IconGenerator();
     final List<FileData> _images = [];
     await _resizeIcons().whenComplete(() async {
-      for (final key in _files.keys) {
-        final _folder = _files[key];
+      for (final key in _iconFiles.keys) {
+        final _folder = _iconFiles[key];
         _images.addAll(_folder.toList());
       }
       // print('Images: ${_images.length}');
@@ -120,12 +120,7 @@ class SetupIcon extends ChangeNotifier {
     });
   }
 
-  Future<bool> _saveFile(
-    String fileName, {
-    // String initialDirectoryDesktop,
-    List<int> binaryData,
-    bool silentErrors = false,
-  }) async {
+  Future<bool> _saveFile(String fileName, {List<int> binaryData, bool silentErrors = false}) async {
     if (kIsWeb) {
       Uri dataUrl;
       try {
@@ -140,41 +135,37 @@ class SetupIcon extends ChangeNotifier {
         return false;
       }
       html.AnchorElement()
-        // ignore: unsafe_html
         ..href = dataUrl.toString()
         ..setAttribute('download', fileName)
         ..click();
       return true;
     }
-    // final Directory _appDocDir = await getApplicationDocumentsDirectory();
-    // final String _appDocPath = _appDocDir.path;
-    io.File(fileName)
+    await getDownloadsDirectory().then((_downloadsDir) => io.File('${_downloadsDir.path}/$fileName')
       ..createSync()
-      ..writeAsBytesSync(binaryData);
-    // ..writeAsStringSync('$_appDocPath/$fileName');
+      ..writeAsBytesSync(binaryData));
     return true;
   }
 
   Future _resizeIcons() async {
-    _files = {};
-    await _generatePngIcons('web', WebIconsFolder());
-    // await _generatePngIcons('iOS', IosIconsFolder());
-    // await _generatePngIcons('macOS', MacOSIconsFolder());
-    // await _generatePngIcons('android', AndroidIconsFolder());
+    if (_iconFiles.isEmpty) {
+      await _generatePngIcons('web', WebIconsFolder());
+      // await _generatePngIcons('iOS', IosIconsFolder());
+      // await _generatePngIcons('macOS', MacOSIconsFolder());
+      // await _generatePngIcons('android', AndroidIconsFolder());
+    }
   }
 
   Future _generatePngIcons(String key, ImageFolder folder) async {
     final img.Image _image = img.decodePng(_icon);
     final IconGenerator _gen = IconGenerator();
     final List<FileData> _archive = await _gen.generateIcons(_image, folder, writeToDiskIO: !kIsWeb);
-    _files[key] = _archive;
-  }
-
-  void _setLoading(bool value) {
-    _loading = value;
-    notifyListeners();
+    _iconFiles[key] = _archive;
   }
 
   bool _loading = false;
   bool get loading => _loading;
+  void _setLoading(bool value) {
+    _loading = value;
+    notifyListeners();
+  }
 }
